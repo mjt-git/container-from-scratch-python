ab82ee1bf9d957f78406f775821477e1f233b5d1
mjt-git <maojt106@gmail.com>
2021-03-07 23:07:41 +0000

    changing to Dice Roller

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import click
+import random
 
 @click.command()
-@click.option("--name")
-def hello(name):
-    click.echo(f'Hello {name}!')
+@click.option("--minval")
+@click.option("--maxval")
+def hello(minval, maxval):
+    click.echo(f'Hello User!')
+    click.echo(f'The minimum value is {minval}')
+    click.echo(f'The maximum value is {maxval}')
+    click.echo(f'************************')
+    click.echo(f'ROLLING THE DICE')
+    click.echo(f'************************')
+    val = random.randint(int(minval), int(maxval))
+    click.echo(f'The Result is {val}')
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
